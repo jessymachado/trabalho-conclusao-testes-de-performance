14270b5cdf071d730a37e88338b049d662d1fbc8
Jessica Machado <jeh-machado@outlook.com>
2025-12-13 17:33:15 -0300

    🧪Automação do agendamento de horários

--- tests/k6/agendamentos.test.js
+++ tests/k6/agendamentos.test.js
@@ -18,11 +18,41 @@ const dados = new SharedArray('agendamentos', () =>
 
 
 export default function () {
-    let token = ''        
-    const user = usuarios[(__VU - 1) % usuarios.length];    
+    let token = ''
+    const idx = (__VU - 1) % dados.length;
+    const user = usuarios[(__VU - 1) % usuarios.length];
 
-    group('Fazendo login com sucesso', function () {        
-        token = efetuarLogin(user);                
+    group('Fazendo login com sucesso', function () {
+        token = efetuarLogin(user);
     });
 
+    group('Marcar agendamento com sucesso', function () {
+        const dias = getProximosDiasUteis();
+        const indiceAleatorio = Math.floor(Math.random() * dias.length);
+        const dataSelecionada = dias[indiceAleatorio];
+
+        const payloadMarcarHorario = {
+            nomeCliente: faker.person.name(),
+            telefoneCliente: faker.person.phone(),
+            dataAgendada: dataSelecionada,
+            horarioAgendado: dados[idx].horario,
+            servico: dados[idx].servico,
+        };
+
+        let responseMarcarAgendamento = http.post(`${BASE_URL}/agendamento/marcar`,
+            JSON.stringify(
+                payloadMarcarHorario),
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+        
+        check(responseMarcarAgendamento, {
+            'status da marcação deve ser 201': (resp) => resp.status === 201,
+            'mensagem deve ser de sucesso': (resp) =>
+                resp.json('message') === 'Agendamento realizado com sucesso!',
+        });
+    });
 }
